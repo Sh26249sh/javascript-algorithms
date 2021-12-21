178174ff27d1334df3446110f1a110d755e701b1
Sveta Shalyupa <Sh26249sh@yandex.ru>
2021-12-22 01:14:51 +0300

    как вариант

--- 14-primes/script.js
+++ 14-primes/script.js
@@ -10,11 +10,11 @@ function primes(num) {
     let mas=[]
     for (i=2;i<=num;i++){
 
-         if (i===2 || i===3 || i===5){
+         if (i===2 || i===3){
             mas.push(i)
         }
-        else if (i%2!==0 && i%3!==0 && i%5!==0){
-        
+        else if (i%2!==0 && i%3!==0){
+        if (Math.sqrt(i)-Math.floor(Math.sqrt(i))>0)
           mas.push(i) 
             
         }
@@ -28,4 +28,4 @@ function primes(num) {
 
 console.log(primes(6)); // [2, 3, 5]
 console.log(primes(17)); // [2, 3, 5, 7, 11, 13, 17]
-console.log(primes(25));
+console.log(primes(100));
